124eeea53aebc1d49a1174d4661cbf92ffb429a3
xioni3 <42072580+xioni3@users.noreply.github.com>
2019-03-02 18:11:22 +0100

    Added MVC interfaces Observer and Subject

--- Subject.h
+++ Subject.h
@@ -5,10 +5,12 @@
 #ifndef ACTIVITY_TRACKER_SUBJECT_H
 #define ACTIVITY_TRACKER_SUBJECT_H
 
+#include "Observer.h"
 
 class Subject {
 public:
-    virtual ~Subject() {}
+    virtual ~Subject() = default;
+
     virtual void notify() = 0;
     virtual void addObserver(Observer* o) = 0;
     virtual void removeObserver(Observer* o) = 0;
